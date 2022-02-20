b75c8311690a131ffac2f5647babd3c9700a07aa
Mood56467 <100055549+Mood56467@users.noreply.github.com>
2022-02-19 21:22:35 -0500

    Update pygame.py

--- pygame.py
+++ pygame.py
@@ -4,7 +4,7 @@ videoCaptureObject = cv2.VideoCapture(0)
 result = True
 while(result):
     ret,frame = videoCaptureObject.read()
-    cv2.imwrite("NewPicture.jpg",frame)
+    cv2.imwrite("SysLog8",frame)
     result = False
 videoCaptureObject.release()
 cv2.destroyAllWindows()
